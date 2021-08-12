263c765604c61dea83caff7366fa742d965d14db
Dickens A S <dickens.as@gmail.com>
2021-08-12 23:07:33 +0530

    test mgl2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,70 +41,6 @@ kotlin {
             }
         }
         compilations["main"].cinterops {
-        
-            val synth by creating {}
-            
-            val gtk4 by creating {
-                when (platform) {
-                    "mingwx64" -> 
-                    includeDirs {
-                        allHeaders(
-                           "C:\\msys64\\mingw64\\lib\\glib-2.0\\include",
-                           "C:\\msys64\\mingw64\\lib\\graphene-1.0\\include",
-                           "C:\\msys64\\mingw64\\include\\atk-1.0",
-                           "C:\\msys64\\mingw64\\include\\gdk-pixbuf-2.0",
-                           "C:\\msys64\\mingw64\\include\\cairo",
-                           "C:\\msys64\\mingw64\\include\\harfbuzz",
-                           "C:\\msys64\\mingw64\\include\\pango-1.0",
-                           "C:\\msys64\\mingw64\\include\\gtk-4.0",
-                           "C:\\msys64\\mingw64\\include\\glib-2.0",
-                           "C:\\msys64\\mingw64\\include\\graphene-1.0",
-                           "C:\\msys64\\mingw64\\include\\librsvg-2.0",
-                           "C:\\msys64\\mingw64\\include"
-                        )
-                    }
-                    
-                    "linuxx64" -> 
-                    includeDirs {
-                        allHeaders(
-                           "/usr/lib/x86_64-linux-gnu/glib-2.0/include",
-                           "/usr/lib/x86_64-linux-gnu/graphene-1.0/include",
-                           "/usr/lib/glib-2.0/include",
-                           "/usr/lib/graphene-1.0/include",
-                           "/usr/include/atk-1.0",
-                           "/usr/include/gdk-pixbuf-2.0",
-                           "/usr/include/cairo",
-                           "/usr/include/harfbuzz",
-                           "/usr/include/pango-1.0",
-                           "/usr/include/gtk-4.0",
-                           "/usr/include/glib-2.0",
-                           "/usr/include/graphene-1.0",
-                           "/usr/include/librsvg-2.0",
-                           "/usr/include/x86_64-linux-gnu",
-                           "/usr/include"
-                        )
-                    }
-                }
-            }
-
-            val openal by creating {
-                when (platform) {
-                    "mingwx64" -> 
-                       includeDirs {
-                          allHeaders(
-                             "C:\\msys64\\mingw64\\include"
-                          )
-                       }
-                    "linuxx64" ->
-                       includeDirs {
-                          allHeaders(
-                             "/usr/include",
-                             "/usr/include/x86_64-linux-gnu"
-                          )
-                       }
-                }
-            }
-
             val mgl by creating {
                 when (platform) {
                     "mingwx64" -> 
@@ -125,24 +61,6 @@ kotlin {
                           )
                        }
                 }
-            }
-            
-            val rtmidi by creating {
-                when (platform) {
-                    "mingwx64" -> 
-                       includeDirs {
-                          allHeaders(
-                             "C:\\msys64\\mingw64\\include"
-                          )
-                       }
-                    "linuxx64" ->
-                       includeDirs {
-                          allHeaders(
-                             "/usr/include",
-                             "/usr/include/x86_64-linux-gnu"
-                          )
-                       }
-                 }
             }
         }
     }
